65a0ebbf62735d56a06320328e4dc504b1ba4ebd
Rômulo Pastore <88741244+Romumoon@users.noreply.github.com>
2022-10-19 22:51:22 -0300

    Update Elixir.exs

--- Elixir.exs
+++ Elixir.exs
@@ -1,3 +1,5 @@
-IO.puts("Hello World")
+defmodule Main do
+  IO.puts("Hello World")
+end
 
 # Complexidade O(1) pois somente printamos uma mensagem na tela, o que acaba deixando nosso programa constante
