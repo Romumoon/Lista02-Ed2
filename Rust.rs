4f6c803c531ce08d6dd70c706df6dcc90f708d0c
Rômulo Pastore <88741244+Romumoon@users.noreply.github.com>
2022-10-19 22:49:01 -0300

    Update Rust.rs

--- Rust.rs
+++ Rust.rs
@@ -2,4 +2,4 @@ fn main() {
     println!("Hello World");
 }
 
-# Complexidade O(1) pois somente printamos uma mensagem na tela, o que acaba deixando nosso programa constante
+//Complexidade O(1) pois somente printamos uma mensagem na tela, o que acaba deixando nosso programa constante
